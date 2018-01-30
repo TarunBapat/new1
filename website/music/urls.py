06125573c55c177cdbccca3582491251c8175b7c
TarunBapat <32245027+TarunBapat@users.noreply.github.com>
2018-01-30 23:51:39 +0530

    added a new project

---
+++ website/music/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, re_path
+from . import views
+
+app_name='music'
+
+urlpatterns = [
+    path('', views.IndexView.as_view(),name='index'),
+    re_path(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(),name='detail'),
+    
+]
